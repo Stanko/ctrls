385cd972c0ad196393843fc601a82f72a0eccef9
Stanko <stanko.tadic@gmail.com>
2025-10-24 12:47:55 +0200

    rng bugfix

--- src/utils/generate-seed.ts
+++ src/utils/generate-seed.ts
@@ -1,10 +1,13 @@
+import Alea from "./alea";
 import random from "./random";
 import { words } from "./words";
+
+const rng = Alea();
 
 export default function generateSeed() {
   return [1, 2, 3]
     .map(() => {
-      const index = random(0, words.length - 1, Math.random, 0);
+      const index = random(0, words.length - 1, rng, 0);
       return words[index];
     })
     .join("-");
